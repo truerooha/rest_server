6cf1af42d1f6cc3ff19e85e5c1f7447f0e5435e6
rooha <github@panadey.ru>
2026-02-11 15:07:41 +0300

    new slot

--- src/utils/order-config.ts
+++ src/utils/order-config.ts
@@ -2,7 +2,7 @@ export const ORDER_CONFIG = {
   orderLeadMinutes: 150,
   deliverySlots: [
     { id: '13:00', time: '13:00' },
-    { id: '17:40', time: '15:40' },
+    { id: '17:43', time: '17:43' },
     { id: '23:00', time: '23:00' },
   ],
 }
