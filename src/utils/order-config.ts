66d2fa9f1a10d651b747c93261f65556d2dbf69e
rooha <github@panadey.ru>
2026-02-23 17:03:52 +0300

    тестовые слоты

--- src/utils/order-config.ts
+++ src/utils/order-config.ts
@@ -7,10 +7,9 @@ export const ORDER_CONFIG = {
   deliveryPriceCentsWhenNotFull: 8300,
   deliverySlots: [
     { id: '13:00', time: '13:00' },
-    { id: '15:00', time: '15:00' },
-    { id: '17:00', time: '17:00' },
-    { id: '19:00', time: '19:00' },
     { id: '21:00', time: '21:00' },
+    { id: '21:30', time: '21:30' },
+    { id: '22:00', time: '22:00' },
     { id: '23:00', time: '23:00' },
   ],
 }
