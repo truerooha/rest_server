55342d5b0e226f3f266e48cdeeb677eb487eb1a4
rooha <github@panadey.ru>
2026-02-23 19:56:51 +0300

    изменён дедлайн и слоты

--- src/utils/order-config.ts
+++ src/utils/order-config.ts
@@ -1,15 +1,13 @@
 import { config } from './config'
 
 export const ORDER_CONFIG = {
-  orderLeadMinutes: 150,
+  orderLeadMinutes: 90,
   minLobbyParticipants: config.minLobbyParticipants,
-  lobbyLeadMinutes: 150,
+  lobbyLeadMinutes: 90,
   deliveryPriceCentsWhenNotFull: 8300,
   deliverySlots: [
     { id: '13:00', time: '13:00' },
-    { id: '21:00', time: '21:00' },
-    { id: '21:30', time: '21:30' },
-    { id: '22:00', time: '22:00' },
-    { id: '23:00', time: '23:00' },
+    { id: '17:00', time: '17:00' },
+
   ],
 }
